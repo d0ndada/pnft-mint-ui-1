15f27835398d83d4d4a518e7b6ab6002e5e59b60
d0ndada <pintoavila09@gmail.com>
2023-07-15 12:59:35 +0200

    creaated the dynamic movements on screen

---
+++ components/ui/nftslide.tsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import "../../styles/globals.css"
+
+export const NftSlide = () => {
+    return (<>
+    <div className='NftWrapper'>
+            <div className="marquee-wrapper1">
+              <div className="marquee1">
+                {[...Array(9)].map((item, index) => (
+                  // eslint-disable-next-line @next/next/no-img-element
+                  <img
+                    className='img1'
+                    key={index}
+                    src={`../../public/assets/${index + 1}.png`}
+                    height={200}
+                    width={200}
+                    alt=""
+                  />
+                ))}
+              </div>
+            </div>
+          </div>
+          <div className='NftWrapper2'>
+            <div className="marquee-wrapper2">
+              <div className="marquee2">
+                {[...Array(9)].map((item, index) => (
+                  // eslint-disable-next-line @next/next/no-img-element
+                  <img
+                    className='img2'
+                    key={index}
+                    src={`../../public/assets/${index + 1}.png`}
+                    height={200}
+                    width={200}
+                    alt=""
+                  />
+                ))}
+              </div>
+            </div>
+          </div></>
+  )
+}
