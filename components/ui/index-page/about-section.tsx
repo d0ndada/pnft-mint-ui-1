b6625cc851e02bf760b3546cc4030db9033f65cc
d0ndada <pintoavila09@gmail.com>
2023-07-28 19:54:21 +0200

    edited to fade up in

--- components/ui/index-page/about-section.tsx
+++ components/ui/index-page/about-section.tsx
@@ -10,13 +10,13 @@ export const AboutSection = () => {
 
   return (
    <section id="about" className="flex h-[100vh] flex-col items-center justify-center space-y-10  p-5 text-foreground scroll-snap-align-start">
-      <h2 ref={ref} className={`mt-[-19%] p-[6%] text-4xl font-bold text-foreground ${inView ? 'animate-fadeIn' : ''}`}>About Us</h2>
+      <h2 ref={ref} className={`mt-[-19%] p-[6%] text-4xl font-bold text-foreground ${inView ? 'animate-fade-in-up' : ''}`}>About Us</h2>
       <div className="flex flex-row">
-        <p className={`max-w-[700px] text-left text-foreground ${inView ? 'animate-fadeIn' : ''}`}>
+        <p className={`max-w-[700px] text-left text-foreground ${inView ? 'animate-fade-in-up' : ''}`}>
           We are a team of passionate individuals committed to creating an outstanding NFT platform. Our mission is to drive the adoption of NFT technology in various industries, and make it accessible to everyone.
         </p>
         <div className="flex justify-center gap-5">
-          <div className={`relative ml-[12%] ${inView ? 'animate-fadeIn' : ''}`}>
+          <div className={`relative ml-[12%] ${inView ? 'animate-fade-in-up' : ''}`}>
             <Image src="/assets/4.png" width={300} height={300} alt="Image description" />
           </div>
         </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -89,11 +89,22 @@ module.exports = {
           "0%": { opacity: "0" },
           "100%": { opacity: "1" },
         },
+        "fade-in-up": {
+          "0%": {
+            opacity: "0",
+            transform: "translateY(20px)",
+          },
+          "100%": {
+            opacity: "1",
+            transform: "translateY(0)",
+          },
+        },
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
         fadeIn: "fadeIn 1s ease-in-out",
+        "fade-in-up": "fade-in-up 1s ease-in-out",
       },
     },
   },
