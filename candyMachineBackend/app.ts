6dd371a116a885de84542849c420a0c626673e66
d0ndada <pintoavila09@gmail.com>
2023-08-02 17:28:57 +0200

    setting up for key

--- candyMachineBackend/app.ts
+++ candyMachineBackend/app.ts
@@ -1,14 +1,21 @@
 
 import { Connection, Keypair, PublicKey } from "@solana/web3.js";
 import { Metaplex, keypairIdentity, bundlrStorage, toMetaplexFile, toBigNumber, CreateCandyMachineInput, DefaultCandyGuardSettings, CandyMachineItem, toDateTime, sol, TransactionBuilder, CreateCandyMachineBuilderContext } from "@metaplex-foundation/js";
+import secret from './keypair.json';
 
 
 if (!process.env.NEXT_PUBLIC_RPC_ENDPOINT) {
     throw new Error("NEXT_PUBLIC_RPC_ENDPOINT is not defined")
 }
+if (!process.env.NEXT_SECRET) {
+    throw new Error("NEXT_SECRET is not defined")
+}
+
+const WALLET = Keypair.fromSecretKey(new Uint8Array(secret))
 
 const SESSION_HASH = 'QNDEMO' + Math.ceil(Math.random() * 1e9);
 const SOLANA_CONNECTION = new Connection(process.env.NEXT_PUBLIC_RPC_ENDPOINT, {
-    
+    commitment: "finalized", httpHeaders: {"x-session-hash": SESSION_HASH}
 })
+
   
