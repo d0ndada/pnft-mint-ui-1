a3103259960947f0d7d1614d7c02e991c050c20a
d0ndada <pintoavila09@gmail.com>
2023-08-02 11:38:45 +0200

    create the collection not done

--- candyMachineBackend/index.js
+++ candyMachineBackend/index.js
@@ -1,1 +1,20 @@
-import {createNft, TokenStandard} from @metaplex
+import { create } from "@metaplex-foundation/mpl-candy-machine"
+import {
+  TokenStandard,
+  createNft,
+} from "@metaplex-foundation/mpl-token-metadata"
+import { generateSigner, percentAmount } from "@metaplex-foundation/umi"
+
+// Create the collection
+const collectionUpdateAuthority = generateSigner(umi)
+const collectionMint = generateSigner(umi)
+await createNft(umi, {
+  mint: collectionMint,
+  authority: collectionUpdateAuthority,
+  name: "Sol Juice NFT",
+  uri: "",
+  sellerFeBasisPoints: percentAmount(9.99, 2),
+  isCollection: true,
+}).sendAndConfirm(umi)
+
+// Create the Candy machine
