3cfb8b9d59036b0971b702103d5041e9485fd059
d0ndada <pintoavila09@gmail.com>
2023-08-02 12:24:04 +0200

    fixing setting & group guard nto done

--- candyMachineBackend/index.js
+++ candyMachineBackend/index.js
@@ -1,16 +1,25 @@
-import { create, mplCandyMachine } from "@metaplex-foundation/mpl-candy-machine"
+import {
+  create,
+  createCandyMachineV2,
+  mplCandyMachine,
+} from "@metaplex-foundation/mpl-candy-machine"
 import {
   TokenStandard,
   createNft,
 } from "@metaplex-foundation/mpl-token-metadata"
-import { generateSigner, percentAmount, some } from "@metaplex-foundation/umi"
+import { dateTime, generateSigner, percentAmount, sol, some } from "@metaplex-foundation/umi"
 import { createUmi } from "@metaplex-foundation/umi-bundle-defaults"
+import { group } from "console"
 
 // initialize umi
-
 const umi = createUmi(
   "https://solana-devnet.g.alchemy.com/v2/8y5XaD-EI4DKbwLDBU4ywF3EnsCoS3kZ"
 ).use(mplCandyMachine())
+
+// const myCustomAuthority = generateSigner(umi)
+// const candyMachineSettings = {
+//   authority: myCustomAuthority,
+// }
 
 // Create the collection
 const collectionUpdateAuthority = generateSigner(umi)
@@ -25,11 +34,23 @@ await createNft(umi, {
 }).sendAndConfirm(umi)
 
 // Create the Candy machine
+
+const candyMachineSettings = {
+  hiddenSettings: none(),
+  configLineSettings: some({
+    prefixName: "My NFT Project #$ID+1$",
+    namelength: 0,
+    prefixUri: "https://arweave.net/",
+    uriLength: 43,
+    isSequential: false,
+  }),
+}
+
 const candyMachine = generateSigner(umi)
-await create(umi, {
+await createCandyMachineV2({
   candyMachine,
   collectionMint: collectionMint.publicKey,
-  collectionUpdateAuthority,
+  collectionUpdateAuthority: umi.identity,
   TokenStandard: TokenStandard.ProgrammableNonFungible,
   sellerFeBasisPoints: percentAmount(9.99, 2),
   itemsAvailable: 5000,
@@ -40,11 +61,34 @@ await create(umi, {
       percentageShare: 100,
     },
   ],
-  configLineSettings: some({
-    prefixName: "",
-    nameLength: 32,
-    prefixUri: "",
-    uriLength: 200,
-    isSequential: false,
-  }),
+  candyMachineSettings,
 }).sendAndConfirm(umi)
+
+
+groups = () => {
+    guards: {
+        botTax: some({lamports: sol(0.001), lastInstruction: true}),
+    },
+    groups: [ 
+        {
+            label: "early",
+            guards: {
+                solPayment: some({ lamports: sol(0.01), destination: treasury }),
+                startDate: some({ date: dateTime("2023-01-20T18:00:00Z") }),
+                allowList: some({ merkleRoot: fica }),// fixa
+                endDate: some({ date: dateTime("2024-10-20T19:00:00Z") }),
+                mintLimit: some({id:1,limit:2}),
+            },
+        },
+        {
+            label: "public",
+            guards: {
+                solPayment: some({ lamports: sol(0.03), destination: treasury }),
+                startDate: some({ date: dateTime("2023-01-20T18:00:00Z") }),
+                // endDate: some({ date: dateTime("2024-10-20T19:00:00Z") }),
+                mintLimit: some({id:2,limit:4}),
+            }
+        }
+
+    ]
+}
