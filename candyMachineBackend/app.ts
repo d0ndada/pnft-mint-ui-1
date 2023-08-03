9bc8ac08cf2f968cf886f2d10a868425b37fd94a
d0ndada <pintoavila09@gmail.com>
2023-08-03 10:21:14 +0200

    tested it crashed made changes

--- candyMachineBackend/app.ts
+++ candyMachineBackend/app.ts
@@ -45,18 +45,27 @@ async function uploadImages(directoryPath: string): Promise<{ uri: string, name:
   for (const fileName of fileNames) {
     const filePath = path.join(directoryPath, fileName);
 
-    // If the file is a JSON file, read the attributes
-  if (path.extname(fileName) === '.json') {
-  const jsonContent = JSON.parse(fs.readFileSync(filePath, 'utf-8'));
+     if (path.extname(fileName) === '.png') {
+        const imgBuffer = fs.readFileSync(filePath);
+        const imgMetaplexFile = toMetaplexFile(imgBuffer, fileName);
 
-  // Find the corresponding entry in nftData and add the attributes
-  for (const data of nftData) {
-    if (data.uri.includes(path.basename(fileName, '.json'))) {
-      data.name = jsonContent.name;
-      data.description = jsonContent.description;
-      data.attributes = jsonContent.attributes;
-      data.properties = jsonContent.properties;
-      break;
+        const imgUri = await METAPLEX.storage().upload(imgMetaplexFile);
+        console.log(`   Image URI:`, imgUri);
+
+        nftData.push({uri: imgUri, name: '', description: '', attributes: [], properties: {}});
+    }
+    // If the file is a JSON file, read the attributes
+    else if (path.extname(fileName) === '.json') {
+        const jsonContent = JSON.parse(fs.readFileSync(filePath, 'utf-8'));
+
+        // Find the corresponding entry in nftData and add the attributes
+        for (const data of nftData) {
+            if (data.uri.includes(path.basename(fileName, '.json'))) {
+                data.name = jsonContent.name;
+                data.description = jsonContent.description;
+                data.attributes = jsonContent.attributes;
+                data.properties = jsonContent.properties;
+                break;
     }
   }
 }
@@ -88,6 +97,21 @@ async function uploadMetadata(data: { uri: string, name: string, description: st
     console.log('   Metadata URI:',uri);
     return uri;  
 }
+// create collection
+async function createCollectionNft(collectionMetadata: any) {
+    const { nft: createCollectionNft } = await METAPLEX.nfts().create({
+        name: collectionMetadata.name,
+        uri: collectionMetadata.uri,
+        sellerFeeBasisPoints: 0,
+        symbol:collectionMetadata.symbol,
+        isCollection: true,
+        updateAuthority: WALLET,
+    });
+          console.log(`✅ - Minted Collection NFT: ${createCollectionNft.address.toString()}`);
+      console.log(`     https://explorer.solana.com/address/${createCollectionNft.address.toString()}?cluster=devnet`);
+
+}
+
 //create 
 async function main() {
     console.log(`Minting NFT's to wallet  ${WALLET.publicKey.toBase58()}. `);
@@ -98,9 +122,9 @@ async function main() {
 
         const metadataUri = await uploadMetadata(data)
         console.log(`Minted NFT with URI ${data.uri} and metadata URI ${metadataUri}`);
-
-        
     }
-    
+    //Upload the collection metadata and create the collection NFT
+    const collectionMetadata:any = JSON.parse(fs.readFileSync("./assets/collection.json","utf-8"))
+    await createCollectionNft(collectionMetadata)
 }
 main()
