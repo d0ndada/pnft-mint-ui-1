3fcd78e71d293c4c55242d8d9233d156b04918e2
d0ndada <pintoavila09@gmail.com>
2023-08-12 16:26:12 +0200

    Team section style

--- components/ui/index-page/team-section.tsx
+++ components/ui/index-page/team-section.tsx
@@ -2,42 +2,48 @@
 import Image from "next/image";
 import { useInView } from 'react-intersection-observer';
 
-
 interface TeamMemberProps {
   image: string;
   name: string;
   role: string;
+  description: string;
 }
 
 export const TeamSection = () => {
   const { ref, inView } = useInView({
-    triggerOnce: true, // Change this to false if you want the animation to trigger again whenever it comes in view
+    triggerOnce: true,
   });
-  function TeamMember({ image, name, role }: TeamMemberProps) {
+
+ function TeamMember({ image, name, role, description }: TeamMemberProps) {
   return (
-    <div className="flex flex-col items-center space-y-2  border-2 border-black">
-      <Image  className="rounded-full" width={300} height={300} src={image} alt={name} />
-      <div className="justify-flex flex w-[100%] flex-col items-center bg-[#ffebcd]">
-      <h3 className="text-xl font-bold">{name}</h3>
-      <p className="text-gray-500">{role}</p>
-   </div>
+    <div className="flex flex-col items-center space-y-1 border-2 border-black">
+      <Image className="" width={225} height={225} src={image} alt={name} />
+      <div className="justify-flex flex w-[100%] flex-col items-center bg-[#ffebcd] ">
+        <h3 className="text-xl font-bold">{name}</h3>
+        <p className="text-lg text-gray-500">{role}</p>
+        <p className="text-m text-gray-600">{description}</p>
       </div>
+    </div>
   );
-
-
 }
+
+
   return (
-   
-    <section id="team" className="flex h-[100vh] flex-col items-center justify-evenly  space-y-8  scroll-snap-align-start">
-      <div>
-  <h2 ref={ref} className={`text-3xl font-bold text-foreground ${inView ? 'animate-fade-in-up' : ''} `}>Our Team</h2>
-  </div>
-        <div  className="grid grid-cols-2 gap-12">
-    <TeamMember image="/assets/0.png" name="John Doe" role="CEO" />
-    <TeamMember image="/assets/2.png" name="Jane Doe" role="CTO" />
-    {/* <TeamMember image="/assets/3.png" name="Someone Else" role="Designer" /> */}
-    {/* Add more team members as needed */}
-  </div>
-</section>
+ <section id="team" className="flex flex-col items-center space-y-8 px-4 py-12 scroll-snap-align-start">
+        <h2 ref={ref} className={`text-3xl font-bold text-foreground mt-[5%] ${inView ? 'animate-fade-in-up' : ''}`}>Our Team</h2>
+        
+        <div className="flex flex-col items-center space-y-8 w-[79%] max-w-6xl"> {/* This is the flex container */}
+            <div className="grid grid-cols-2 gap-12 ">
+                <TeamMember image="/assets/0.png" name="John Doe" role="Lead Developer" description="The technical brain behind [Project Name]. Responsible for all technical aspects, from setting up the CandyMachine to crafting the user interface." />
+                <TeamMember image="/assets/2.png" name="Jane Doe" role="Lead Artist" description="The creative genius behind our unique NFT images. Crafted each piece with a keen eye for detail and a passion for [specific art style or theme]." />
+                {/* Add more team members as needed */}
+            </div>
+
+            <div className="flex flex-col items-center space-y-2 border-2 border-black bg-[#ffebcd] p-4  md:w-2/3"> {/* Adjusted width for responsiveness */}
+                <h3 className="text-xl font-bold text-foreground">Transparency Note</h3>
+                <p className="text-sm text-foreground">We value transparency and trust. Our team is currently using pseudonyms for privacy reasons. As our project grows and evolves, we&apos;ll provide more details about our team and our backgrounds.</p>
+            </div>
+        </div>
+    </section>
   )
 }
