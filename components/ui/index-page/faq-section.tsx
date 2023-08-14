c11f10834452e29167f97c0ada6e859037c9ddee
d0ndada <pintoavila09@gmail.com>
2023-08-14 11:54:00 +0200

    style faq

--- components/ui/index-page/faq-section.tsx
+++ components/ui/index-page/faq-section.tsx
@@ -2,6 +2,7 @@
 import { Icons } from "@/components/icons";
 import { useEffect, useState } from "react";
 import { useInView } from 'react-intersection-observer';
+import { faqs } from "./question";
 
 interface FaqEventProps {
   question: string;
@@ -13,18 +14,7 @@ export const FaqSection = () => {
    const { ref, inView } = useInView({
     triggerOnce: true, // Change this to false if you want the animation to trigger again whenever it comes in view
   });
-  const faqs = [
-    {
-      question: "I've never purchased an NFT before, tell me where to start?",
-      answer: "The first thing you should do is download MetaMask, a browser plugin that will act as your interface to the Ethereum Network. You'll also want some ETH, which can be purchased via a fiat on-ramp such as Coinbase or PayPal. From there, you'll send the ETH to your MetaMask address and will be able to use any DeFi service that connects to MetaMask!"
-    },
-    {
-      question: "How many dogs will there be?",
-      answer: "There will be 8888 Dogs."
-    },
-    // Add more FAQs as needed
-  
-  ];
+
   function FAQ({ question, answer }: FaqEventProps) {
       const [isOpen, setIsOpen] = useState(false);
     // Close other FAQ items when one is opened
@@ -34,17 +24,17 @@ export const FaqSection = () => {
         }
     }, [isOpen]);
        return (
-        <div className={`mb-4 p-4 rounded-lg transition-colors shadow-md ${isOpen ? 'bg-[#ffebcd]' : 'hover:bg-[#f9e5c9]'}`}>
+        <div className={`mb-4 p-4  md:w-[650px] lg:w-[700px] xl:w-[789px] 2xl:w-[786px] rounded-lg transition-colors shadow-md bg-card ${isOpen ? 'bg-popover' : 'hover:bg-popover'}`}>
          <button 
-                className={`flex cursor-pointer items-center text-[1.3rem] font-bold hover:text-primary hover:underline transition-colors duration-300 `} 
+                className={`flex cursor-pointer justify-between w-full items-center text-[1.3rem] font-bold hover:text-primary hover:underline transition-colors duration-300 `} 
                 onClick={() => setIsOpen(!isOpen)}
             >{question}
           <span className={`inline-block ml-2 transition-transform duration-300 ${isOpen ? 'rotate-180' : ''}`}>
             <Icons.down/>
           </span>
         </button>
-        <div className={`mt-2 overflow-hidden transition-all duration-500 ease-in-out ${isOpen ? 'max-h-[1000px]' : 'max-h-0'}`}>
-          {isOpen && <p className="mt-[4%] text-m  ">{answer}</p>}
+        <div className={`mt-2 overflow-hidden transition-all duration-500 ease-in-out ${isOpen ? 'max-h-[1000px] opacity-1' : 'max-h-0 opacity-0'}`}>
+          {isOpen && <p className="mt-[4%] text-m">{answer}</p>}
         </div>
       </div>
     );
@@ -52,8 +42,8 @@ export const FaqSection = () => {
 
   return (
       <section id="faq" className="flex h-[100vh] flex-col items-center justify-center  p-8 scroll-snap-align-start" >
-      <h2 ref={ref}  className={`mb-8 mt-[-25%] text-3xl font-bold text-foreground ${inView ? 'animate-fade-in-up' : ''}  `}>FAQ</h2>
-      <div ref={ref} className={`flex max-h-[71px] max-w-[649px] flex-col items-start ${inView ? 'animate-fade-in-up' : ''}`}>
+      <h2 ref={ref}  className={`mb-8 mt-[-5%] text-3xl font-bold text-foreground ${inView ? 'animate-fade-in-up' : ''}  `}>FAQ</h2>
+      <div ref={ref} className={`flex  max-h-[42vh] max-w-[786px] w-[80%] flex-col items-start ${inView ? 'animate-fade-in-up' : ''}`}>
       {faqs.map((faq, index) => (
         <FAQ key={index} question={faq.question} answer={faq.answer} />
       ))}
---
+++ components/ui/index-page/question.tsx
@@ -0,0 +1,18 @@
+export const faqs = [
+  {
+    question: "What sets Solar Juice apart from other NFT projects?",
+    answer: "Solar Juice is not just another NFT collection; it's a celebration of solar energy's power and potential. Each NFT is a digital masterpiece, intricately designed to capture the essence of solar energy, making it a unique blend of art and environmental consciousness on the Solana blockchain."
+  },
+  {
+    question: "How does Solar Juice contribute to the environment?",
+    answer: "Beyond being inspired by solar energy, a portion of every Solar Juice NFT minted goes towards supporting renewable energy initiatives and projects. By owning a Solar Juice NFT, you're not just acquiring a digital asset; you're making a statement for a cleaner, greener future."
+  },
+  {
+    question: "Are there any benefits for early adopters of Solar Juice NFTs?",
+    answer: "Absolutely! Early adopters often enjoy exclusive access to special events, rare NFT drops, and community-driven activities. By being a part of our early community, you get to shape the future of Solar Juice and enjoy the rewards of being a pioneer."
+  },
+  {
+    question: "How can I stay updated with the latest news and drops from Solar Juice?",
+    answer: "The best way to stay in the loop is by joining our community on Discord and following us on our social media channels. We regularly share updates, announcements, and exclusive content with our community members."
+  },
+]
