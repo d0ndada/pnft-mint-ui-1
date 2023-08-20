b4f24476298cbaa68e22adcaa628a4519bdb627f
d0ndada <pintoavila09@gmail.com>
2023-08-20 17:13:45 +0200

    typo

--- config/site.ts
+++ config/site.ts
@@ -1,7 +1,7 @@
 export type SiteConfig = typeof siteConfig
 
 export const siteConfig = {
-  name: "Soljuice",
+  name: "SolJuice",
   description: "MintUI with Radix UI and Tailwind CSS.",
   mainNav: [
     {
