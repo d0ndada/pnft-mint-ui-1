3b5a3c592487a685c6406a7ab16392c29901d8a2
d0ndada <pintoavila09@gmail.com>
2023-10-01 14:54:41 +0200

    on merge got old code now new

--- components/ui/minting/mint-button.tsx
+++ components/ui/minting/mint-button.tsx
@@ -1,4 +1,6 @@
-import { useState } from "react"
+"use client"
+
+import { useEffect, useState } from "react"
 import {
   CandyGuard,
   CandyMachine,
@@ -13,12 +15,14 @@ import {
 import {
   DigitalAsset,
   TokenStandard,
+  fetchAllDigitalAssetByOwner,
   fetchDigitalAsset,
 } from "@metaplex-foundation/mpl-token-metadata"
 import { setComputeUnitLimit } from "@metaplex-foundation/mpl-toolbox"
 import {
   KeypairSigner,
   PublicKey,
+  Transaction,
   TransactionBuilder,
   generateSigner,
   none,
@@ -32,7 +36,8 @@ import { getAllowListByGuard } from "@/lib/mintsettings"
 import { useUmi } from "@/hooks/useUmi"
 import { Button } from "@/components/ui/button"
 
-import { useToast } from "./use-toast"
+import { useToast } from "../toast/use-toast"
+import { toWeb3JsTransaction } from "@metaplex-foundation/umi-web3js-adapters"
 
 type MintButtonProps = React.ComponentProps<typeof Button> & {
   group?: string
@@ -44,6 +49,7 @@ type MintButtonProps = React.ComponentProps<typeof Button> & {
   onMintCallback?: (mint?: DigitalAsset, signature?: string) => void
   setMessageCallback?: (message?: string) => void
   setDisabledCallback?: (disabled?: boolean) => void
+  mintLimit?: number,
 }
 
 export function MintButton({
@@ -57,20 +63,49 @@ export function MintButton({
   nftGateMint,
   setDisabledCallback,
   setMessageCallback,
+  mintLimit,
   ...props
 }: MintButtonProps) {
   const { toast } = useToast()
   const umi = useUmi()
+  const wallet = umi?.identity.publicKey
   const [loading, setLoading] = useState(false)
+  const [mintAmount, setMintAmount] = useState(1)
+  const [mintedByYou, setMintedByYou] = useState(0)
+  
+  useEffect(() => {
+    const fetchNFTs = async () => {
+    if(umi && wallet){
+    const ownedNft = await fetchAllDigitalAssetByOwner(umi, wallet);
+
+    const ownOfCollection = ownedNft.filter(
+      (nft) =>
+        nft.metadata.collection.__option === "Some" &&
+        nft.metadata.collection.value.key === process.env.NEXT_PUBLIC_COLLECTION_MINT
+    );
+
+    console.log(ownOfCollection.length);
+      setMintedByYou(ownOfCollection.length);
+      // console.log(mintedByYou);
+      
+  };
+}
+   fetchNFTs();
+}, [umi, wallet]); // Empty dependency array means this effect runs once on mount
 
   const mintBtnHandler = async () => {
     if (!candyMachine) {
       return
     }
 
-    try {
-      setLoading(true)
+    const nftMintArr: KeypairSigner[] = [];
 
+    let txArray: Transaction[] = [];
+    let lastSignature: Uint8Array | undefined;
+
+    for (let i = 0; i < mintAmount; i++) {
+  
+      setLoading(true)
       const mintArgs: Partial<DefaultGuardSetMintArgs> = {}
       console.log(guardToUse)
       //TODO: Implement rest of guard logic NFT BURN, NFT Payment, FreezeSolPayment FreezeTokenPayment etc also consolidate this logic, very cluttered
@@ -104,7 +139,7 @@ export function MintButton({
               duration: 5000,
             })
             setDisabledCallback && setDisabledCallback(true)
-            return
+            break
           }
         } else {
           toast({
@@ -137,7 +172,7 @@ export function MintButton({
               duration: 5000,
             })
             setDisabledCallback && setDisabledCallback(true)
-            return
+            break
           }
         }
         mintArgs.mintLimit = some({
@@ -218,59 +253,85 @@ export function MintButton({
           })
         }
       }
+      //Todo: for multimint, probably move these to their own txns
       const nftSigner = generateSigner(umi)
-      const mintV2Builder = mintV2(umi, {
-        candyMachine: candyMachine.publicKey,
-        collectionMint: candyMachine.collectionMint,
-        collectionUpdateAuthority: candyMachine.authority,
-        nftMint: nftSigner,
-        minter: umi.identity,
-        candyGuard: candyGuard?.publicKey,
-        mintArgs: mintArgs,
-        group: group ? group : undefined,
-        tokenStandard: TokenStandard.ProgrammableNonFungible,
-      })
+      nftMintArr.push(nftSigner);
 
-      let tx = transactionBuilder()
-        .add(setComputeUnitLimit(umi, { units: 600_000 }))
-        .add(mintV2Builder)
-      if (routeBuilder) {
-        //Make sure route ix comes first
-        tx = routeBuilder
-          .add(setComputeUnitLimit(umi, { units: 600_000 }))
-          .add(mintV2Builder)
-      }
-      const { signature } = await tx.sendAndConfirm(umi, {
-        confirm: { commitment: "finalized" },
-        send: {
-          skipPreflight: true,
-        },
-      })
-
-      const nft = await fetchDigitalAsset(umi, nftSigner.publicKey).catch(
-        (err) => {
-          console.log(err)
-          return undefined
-        }
-      )
-      onMintCallback &&
-        onMintCallback(nft, base58.deserialize(signature).toString())
-      if (nft) {
-        toast({
-          title: "Minted!",
-          description: `You minted ${nft?.metadata?.name}!`,
+      const tx = transactionBuilder().add(
+        mintV2(umi, {
+          candyMachine: candyMachine.publicKey,
+          collectionMint: candyMachine.collectionMint,
+          collectionUpdateAuthority: candyMachine.authority,
+          nftMint: nftSigner,
+          minter: umi.identity,
+          candyGuard: candyGuard?.publicKey,
+          mintArgs: mintArgs,
+          group: group ? group : undefined,
+          tokenStandard: TokenStandard.ProgrammableNonFungible,
         })
-      }
-    } catch (err: any) {
-      console.error(err)
-    } finally {
-      setLoading(false)
-    }
-  }
+      )
+        .prepend(setComputeUnitLimit(umi, { units: 800_000 }))
+        
+    
+      if (!tx) return;
+      const builtTransaction = await tx.buildWithLatestBlockhash(umi);
+      const signedTransaction = await nftSigner.signTransaction(builtTransaction);
 
+      txArray.push(signedTransaction);
+
+
+   
+    }
+     // Batch and send all transactions together
+        const signedTxs = await umi.identity.signAllTransactions(txArray);
+        for (let signedTx of signedTxs) {
+            await umi.rpc.sendTransaction(signedTx);
+        }
+
+        toast({
+            title: "Mint successful!",
+            description: `You minted ${mintAmount} NFTs.`,
+            // status: "success",
+            duration: 5000,
+        });
+
+  }
+  
   return (
-    <Button className={className} onClick={mintBtnHandler} {...props}>
-      {loading ? "Minting..." : "Mint"}
-    </Button>
+    <div className="flex items-center justify-end">
+      <div className="relative  flex h-10 w-32 flex-row rounded-lg bg-transparent">
+        <Button
+          data-action="decrement"
+  className="h-full w-20 cursor-pointer rounded-l border-inc-dec-border bg-inc-dec-bg text-inc-dec-text hover:scale-105 hover:bg-inc-dec-hover hover:text-inc-dec-textHover"
+          onClick={() => mintAmount > 1 && setMintAmount((prev) => prev - 1)}
+        >
+          -
+        </Button>
+        <input
+          type="text"
+  className="text-md flex w-full cursor-default items-center rounded border-inc-dec-border bg-inc-dec-bg text-center font-semibold text-inc-dec-text hover:bg-inc-dec-hover hover:text-inc-dec-textHover md:text-base"
+          name="custom-input-number"
+          value={mintAmount}
+          readOnly
+        />
+        <Button
+          data-action="increment"
+  className="h-full w-20 cursor-pointer rounded-r  border-inc-dec-border bg-inc-dec-bg text-inc-dec-text hover:scale-105 hover:bg-inc-dec-hover hover:text-inc-dec-textHover"
+          onClick={() =>
+            mintAmount < Number(mintLimit) - Number(mintedByYou) &&
+            setMintAmount((prev) => prev + 1)
+          }
+        >
+          +
+        </Button>
+      </div>
+      <Button
+        className={`${className} ml-3 bg-mintButton-bg text-mintButton-text transition-all  duration-300 ease-in-out hover:scale-105 hover:bg-mintButton-hover active:bg-mintButton-active disabled:bg-mintButton-disabled`}
+        onClick={mintBtnHandler}
+        {...props}
+      >
+        {loading ? "Minting..." : "Mint"}
+      </Button>
+    </div>
   )
 }
