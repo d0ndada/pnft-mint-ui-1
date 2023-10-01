1324283e3a17ae36cb27a842b7c22b7c6f8f2e5c
d0ndada <pintoavila09@gmail.com>
2023-10-01 14:20:02 +0200

    animation on ladnign page working

--- app/page.tsx
+++ app/page.tsx
@@ -34,7 +34,7 @@ export default function IndexPage() {
     // md:py-10, pt-6, pl-[40%]
     <>
     <section  className="custom-container  flex h-[100vh] flex-col items-center gap-6 bg-main px-4  pb-8 scroll-snap-align-start ">
-        <div ref={ref} className={`mt-[100px] flex max-w-[100%] animate-fadeIn flex-col items-center gap-2 ${!inView ? 'translate-y-5 opacity-0' : 'animate-fade-in-up'}`}>
+        <div ref={ref} className={`mt-[100px] flex max-w-[100%]  flex-col items-center gap-2 ${!inView ? 'translate-y-5 opacity-0' : 'animate-fade-in-up'}`}>
           <div>
             <h1 className=" mt-[5%] text-3xl font-extrabold leading-tight tracking-tighter text-textColor md:text-4xl ">
               Welcome to SolJuice: Harness the Power of Solar NFTs!
