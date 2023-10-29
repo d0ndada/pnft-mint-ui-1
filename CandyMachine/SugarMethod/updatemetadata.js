42cb99cd1b71a44b430d7ceb507d119e681931f0
d0ndada <pintoavila09@gmail.com>
2023-10-29 14:59:25 +0100

    editing test nfts

--- CandyMachine/SugarMethod/updatemetadata.js
+++ CandyMachine/SugarMethod/updatemetadata.js
@@ -3,10 +3,52 @@ const path = require("path")
 
 const assetsDir = path.join(__dirname, "assets")
 
-// Load rarity data
-const rarityData = require("./rarityData.json")
-const percentageRarity = rarityData.percentageRarity
-const rarityTiers = rarityData.rarityTiers
+const additionalAttributes = [
+  {
+    trait_type: "Maximal Power (Pmax)",
+    value: "420, 319, 462, and 504 watt",
+  },
+  {
+    trait_type: "MPP Voltage (Vmpp)",
+    value: "31.50, 30.20, 38.14, and 38.18 volt",
+  },
+  {
+    trait_type: "MPP Current (Impp)",
+    value: "13.34, 10.56, 15.43, and 16.79 ampere",
+  },
+  {
+    trait_type: "Open Circuit Voltage (Voc)",
+    value: "38.10, 36.60, 31.54, and 31.58 volt",
+  },
+  {
+    trait_type: "Short Circuit Current (Isc)",
+    value: "14.07, 11.34, 14.65, and 15.96 ampere",
+  },
+  {
+    trait_type: "Cell Type",
+    value: "N-Type Mono 182mm x 91mm",
+  },
+  {
+    trait_type: "Number of Cells",
+    value: "108 cells (6 x 18)",
+  },
+  {
+    trait_type: "Dimensions",
+    value: "1722 x 1134 x 30 mm",
+  },
+  {
+    trait_type: "Weight",
+    value: "24.0 kg",
+  },
+  {
+    trait_type: "Snow Load",
+    value: "Up to 5400 Pa",
+  },
+  {
+    trait_type: "Wind Load",
+    value: "Up to 2400 Pa",
+  },
+]
 
 fs.readdir(assetsDir, (err, files) => {
   if (err) {
@@ -56,101 +98,23 @@ fs.readdir(assetsDir, (err, files) => {
 
       // Calculate rarity score and assign tier
       if (jsonData.attributes) {
-        jsonData.attributes.forEach((attribute) => {
-          //   const rarityValue = parseFloat(
-          //     percentageRarity[
-          //       `${attribute.trait_type}: ${attribute.value}`
-          //     ]?.replace("%", "") || 0
-          //   )
-          //   attribute.rarity = rarityValue
-
-          //   if (
-          //     rarityTiers.Legendary.includes(
-          //       `${attribute.trait_type}: ${attribute.value}`
-          //     )
-          //   ) {
-          //     attribute.rarityTier = "Legendary"
-          //   } else if (
-          //     rarityTiers.Rare.includes(
-          //       `${attribute.trait_type}: ${attribute.value}`
-          //     )
-          //   ) {
-          //     attribute.rarityTier = "Rare"
-          //   } else {
-          //     attribute.rarityTier = "Common"
-          //   }
-          delete attribute.rarity
-          delete attribute.rarityTier
-        })
+        jsonData.attributes.forEach((attribute) => {})
       }
       delete jsonData.rarityScore
       delete jsonData.rarityTier
       delete jsonData.attributeRarities
       delete jsonData.animation_url
       delete jsonData.external_url
+
       jsonData.collection = {
         name: "SolarJuice Collection",
         family: "SolarJuice",
         description: "Collection of 3000 solar panels on the blockchain.",
       }
+      jsonData.attributes = jsonData.attributes.concat(additionalAttributes)
 
       // Write the updated JSON back to the file
       fs.writeFileSync(filePath, JSON.stringify(jsonData, null, 2), "utf-8")
     }
   })
 })
-
-// Change the name because of error
-
-// const fs = require("fs")
-// const path = require("path")
-
-// const assetsDir = path.join(__dirname, "assets")
-
-// fs.readdir(assetsDir, (err, files) => {
-//   console.log("Script starting")
-//   if (err) {
-//     console.error("Error reading the directory:", err)
-//     return
-//   }
-//   console.log("Files detected:", files)
-
-//   files.forEach((file) => {
-//     const fileExtension = path.extname(file)
-//     const baseName = path.basename(file, fileExtension)
-
-//     // Check if the base name is an integer
-//     if (Number.isInteger(Number(baseName))) {
-//       console.log(`Processing file: ${file}`)
-
-//       // If it's a JSON file, update the metadata
-//       if (fileExtension === ".json") {
-//         const filePath = path.join(assetsDir, file)
-//         const fileContent = fs.readFileSync(filePath, "utf-8")
-//         let jsonData = JSON.parse(fileContent)
-
-//         // Update the name, image, and uri fields
-//         jsonData.name = `Solar Juice ${baseName}`
-//         jsonData.image = `${baseName}.png`
-//         if (
-//           jsonData.properties &&
-//           jsonData.properties.files &&
-//           jsonData.properties.files[0]
-//         ) {
-//           jsonData.properties.files[0].uri = `${baseName}.png`
-//         }
-
-//         // Remove specified fields
-//         delete jsonData.collection
-//         if (jsonData.properties) {
-//           delete jsonData.properties.category
-//           delete jsonData.properties.creators
-//         }
-
-//         // Write the updated JSON back to the file
-//         fs.writeFileSync(filePath, JSON.stringify(jsonData, null, 2), "utf-8")
-//       }
-//     }
-//   })
-//   console.log("Script ending")
-// })
